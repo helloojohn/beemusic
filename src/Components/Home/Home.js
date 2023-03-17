f015b05200c2d18f028e5354278ff22834ab1783
Khodepanda <khodepanda@gmail.com>
2023-03-17 21:56:57 +0100

    Update file

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import "../Home/Home.css"
-
+import React from "react";
+import "../Home/Home.css";
+// import { Main } from "../Sections/Main/Main";
 import { LeftMenu } from "../Sections/Menu/LeftMenu";
+// import { Sidebar } from "../Sections/Aside/Sidebar";
 
 function Home() {
   return (
-      <div className='home'>
-          <LeftMenu />
+    <div className="home">
+      <LeftMenu />
+      {/* <Main />
+      <Sidebar /> */}
     </div>
-  )
+  );
 }
 
 export { Home };
---
+++ src/Components/Sections/Main/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../Main/Main.css";
+
+function Main() {
+  return (
+    <div className="main">
+      <h1>main</h1>
+    </div>
+  );
+}
+
+export { Main };
